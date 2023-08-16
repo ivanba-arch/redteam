66e28d82cfce1a522d973fded8545414fcee5cdc
Alan Bautista <41350198+ivanba-arch@users.noreply.github.com>
2023-08-16 03:07:02 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,5 +8,6 @@ pwsh -Command "IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-at
 sudo mkdir -p /AtomicRedTeam/atomics/
 sudo cp -r TR-Blogs/atomic-red-docker/atomics-overrides /AtomicRedTeam/atomics
 sudo cp TR-Blogs/atomic-red-docker/RunTests.ps1 /root
-echo 'pwsh -Command "IEX (IWR ''https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1'' -UseBasicParsing); Install-AtomicRedTeam -getAtomics" -Force' >> /home/ubuntu/.bashrc
+#echo 'pwsh -Command "IEX (IWR ''https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1'' -UseBasicParsing); Install-AtomicRedTeam -getAtomics" -Force' >> /home/ubuntu/.bashrc
+echo 'curl -o /tmp/install_remote.sh https://raw.githubusercontent.com/ivanba-arch/redteam/main/install_d.sh && chmod +x /tmp/install_remote.sh && /tmp/install_remote.sh' >> /home/ubuntu/.bashrc
 echo "Script completed" > /tmp/script_status.log
