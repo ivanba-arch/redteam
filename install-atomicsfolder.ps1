d8076c2b4bedea24d6e697351cedf2c2c16e31cb
Alan Bautista <41350198+ivanba-arch@users.noreply.github.com>
2023-08-16 02:19:39 -0300

    Update install-atomicsfolder.ps1

--- install-atomicsfolder.ps1
+++ install-atomicsfolder.ps1
@@ -1,5 +1,5 @@
 function Install-AtomicsFolder {
-  
+
     <#
     .SYNOPSIS
 
@@ -40,20 +40,21 @@ function Install-AtomicsFolder {
         [string]$DownloadPath = $InstallPath,
 
         [Parameter(Mandatory = $False, Position = 2)]
-        [string]$RepoOwner = "redcanaryco",
+        [string]$RepoOwner = "ivanba-arch",  # Cambiar al nombre de tu usuario de GitHub
 
         [Parameter(Mandatory = $False, Position = 3)]
-        [string]$Branch = "master",
+        [string]$Branch = "main",  # Cambiar al nombre de la rama correspondiente
 
         [Parameter(Mandatory = $False)]
-        [switch]$Force = $False, # delete the existing install directory and reinstall
+        [switch]$Force = $False, # eliminar la carpeta atomics existente antes de la instalación si existe
 
         [Parameter(Mandatory = $False)]
         [switch]$NoPayloads = $False
     )
+
     Try {
         $InstallPathwAtomics = Join-Path $InstallPath "atomics"
-        if ($Force -or -Not (Test-Path -Path $InstallPathwAtomics )) {
+        if ($Force -or -Not (Test-Path -Path $InstallPathwAtomics)) {
             write-verbose "Directory Creation"
             if ($Force) {
                 Try { 
@@ -66,74 +67,21 @@ function Install-AtomicsFolder {
             }
             if (-not (Test-Path $InstallPath)) { New-Item -ItemType directory -Path $InstallPath | Out-Null }
 
-            $url = "https://github.com/$RepoOwner/atomic-red-team/archive/$Branch.zip"
-            $path = Join-Path $DownloadPath "$Branch.zip"
+            $url = "https://raw.githubusercontent.com/$RepoOwner/redteam/main/install-atomicsfolder.ps1"  # Cambiar la URL a la ubicación correcta de tu repositorio
+            $path = Join-Path $DownloadPath "$Branch-install-atomicsfolder.ps1"  # Cambiar el nombre del archivo de acuerdo con tu estructura
+
             [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-            write-verbose "Beginning download of atomics folder from Github"
-			
-            # disable progress bar for faster performances
-            $ProgressPreference_backup = $global:ProgressPreference
-            $global:ProgressPreference = "SilentlyContinue"
-			
-            if ($NoPayloads) {
-                # download zip to memory and only extract atomic yaml files
-                # load ZIP methods
-                Write-Host -ForegroundColor Yellow "Reading the Atomic Red Team repo into a memory stream. This could take up to 3 minutes."
-                Add-Type -AssemblyName System.IO.Compression.FileSystem
-                [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression') | Out-Null
+            write-verbose "Beginning download of atomics folder script from Github"
+            Invoke-WebRequest $url -OutFile $path
 
-                # read github zip archive into memory
-                $ms = New-Object IO.MemoryStream
-                [Net.ServicePointManager]::SecurityProtocol = ([Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12)
+            write-verbose "Running atomics folder script"
+            . $path  # Ejecutar el script de la carpeta atomics descargado
 
-                $httpClient = New-Object System.Net.Http.HttpClient
-                $httpClient.Timeout = New-Object System.TimeSpan(0, 3, 0)
-                $response = $httpClient.GetAsync($url).Result
-                $response.Content.CopyToAsync($ms).Wait()
-                $zip = New-Object System.IO.Compression.ZipArchive($ms)
-
-                $Filter = '*.yaml'
-
-                # ensure the output folder exists
-                $exists = Test-Path -Path $InstallPathwAtomics
-                if ($exists -eq $false) {
-                    $null = New-Item -Path $InstallPathwAtomics -ItemType Directory -Force
-                }
-
-                # find all files in ZIP that match the filter (i.e. file extension)
-                $zip.Entries | 
-                Where-Object { 
-                        ($_.FullName -like $Filter) `
-                        -and (($_.FullName | split-path | split-path -Leaf) -eq [System.IO.Path]::GetFileNameWithoutExtension($_.Name)) `
-                        -and ($_.FullName | split-path | split-path | split-path -Leaf) -eq "atomics"
-                } |
-                ForEach-Object { 
-                    # extract the selected items from the ZIP archive
-                    # and copy them to the out folder
-                    $dstDir = Join-Path $InstallPathwAtomics ($_.FullName | split-path | split-path -Leaf)
-                    New-Item -ItemType Directory -Force -Path $dstDir | Out-Null
-                    [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, (Join-Path $dstDir $_.Name), $true)
-                }
-                $zip.Dispose()
-            }
-            else {
-                Invoke-WebRequest $url -OutFile $path
-
-                write-verbose "Extracting ART to $InstallPath"
-                $zipDest = Join-Path "$DownloadPath" "tmp"
-                Microsoft.PowerShell.Archive\Expand-Archive -LiteralPath $path -DestinationPath "$zipDest" -Force:$Force
-                $atomicsFolderUnzipped = Join-Path (Join-Path $zipDest "atomic-red-team-$Branch") "atomics"
-                Move-Item $atomicsFolderUnzipped $InstallPath
-                Remove-Item $zipDest -Recurse -Force
-                Remove-Item $path
-            }
-			
-            # restore progress bar preferences
-            $global:ProgressPreference = $ProgressPreference_backup
+            Write-Host "Atomic Red Team Atomics Folder installation is complete." -Fore Yellow
         }
         else {
             Write-Host -ForegroundColor Yellow "An atomics folder already exists at $InstallPathwAtomics. No changes were made."
-            Write-Host -ForegroundColor Cyan "Try the install again with the '-Force' parameter if you want to delete the existing installion and re-install."
+            Write-Host -ForegroundColor Cyan "Try the installation again with the '-Force' parameter if you want to delete the existing installation and re-install."
             Write-Host -ForegroundColor Red "Warning: All files within the atomics folder ($InstallPathwAtomics) will be deleted when using the '-Force' parameter."
         }
     }
