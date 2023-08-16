5ca2bdd3b58bf69fac7b1b16734cc3ae2f3ae4ea
Alan Bautista <41350198+ivanba-arch@users.noreply.github.com>
2023-08-16 02:31:29 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ sudo apt-get update
 sudo snap install powershell --classic
 sudo apt-get install git
 git clone https://github.com/sysdig/TR-Blogs.git
-#pwsh -Command "IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing); Install-AtomicRedTeam -getAtomics" -Force > /tmp/pwsh_output.log 2>&1
-pwsh -Command "IEX (IWR 'https://raw.githubusercontent.com/ivanba-arch/redteam/main/install-atomicredteam.ps1' -UseBasicParsing); Install-AtomicRedTeam -getAtomics" -Force > /tmp/pwsh_output.log 2>&1
+pwsh Install-Module -Name powershell-yaml -Scope CurrentUser -Force
+pwsh -Command "IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing); Install-AtomicRedTeam -getAtomics" -Force > /tmp/pwsh_output.log 2>&1
 sudo mkdir -p /AtomicRedTeam/atomics/
 sudo cp -r TR-Blogs/atomic-red-docker/atomics-overrides /AtomicRedTeam/atomics
 sudo cp TR-Blogs/atomic-red-docker/RunTests.ps1 /root
