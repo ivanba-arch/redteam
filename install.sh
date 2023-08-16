eb3e05abce71ffe88aae5ec6fa1114a7ff3b6660
Alan Bautista <41350198+ivanba-arch@users.noreply.github.com>
2023-08-16 00:36:24 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo snap install powershell --classic
+sudo apt-get install git
+git clone https://github.com/sysdig/TR-Blogs.git
+pwsh -Command "IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing); Install-AtomicRedTeam -getAtomics" -Force
+mkdir -p /AtomicRedTeam/atomics/
+sudo cp -r TR-Blogs/atomic-red-docker/atomics-overrides /AtomicRedTeam/atomics
+sudo cp TR-Blogs/atomic-red-docker/RunTests.ps1 /root
