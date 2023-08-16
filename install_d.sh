a30f90530fe3e3381cffa3786735ba4f9727064f
Alan Bautista <41350198+ivanba-arch@users.noreply.github.com>
2023-08-16 03:11:10 -0300

    Update install_d.sh

--- install_d.sh
+++ install_d.sh
@@ -2,6 +2,4 @@
 clear
 echo    LOADING . . .
 pwsh -Command "IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing); Install-AtomicRedTeam -getAtomics" -Force > /tmp/pwsh_output.log 2>&1
-#echo 'pwsh -Command "IEX (IWR ''https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1'' -UseBasicParsing); Install-AtomicRedTeam -getAtomics" -Force' >> /home/ubuntu/.bashrc
-echo 'curl -o /tmp/install_remote.sh https://raw.githubusercontent.com/ivanba-arch/redteam/main/install_d.sh && chmod +x /tmp/install_remote.sh && /tmp/install_remote.sh' >> /home/ubuntu/.bashrc
 echo "Script completed" > /tmp/script_status.log
