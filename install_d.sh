2db0eb2e143bf3a351599632c0e30f3278894f4e
Alan Bautista <41350198+ivanba-arch@users.noreply.github.com>
2023-08-16 03:28:25 -0300

    Update install_d.sh

--- install_d.sh
+++ install_d.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 clear
-echo    LOADING . . .
-echo 
+echo
+echo
+echo
+echo           /$$$$$$ /$$   /$$ /$$    /$$  /$$$$$$  /$$   /$$ /$$$$$$$$        /$$$$$$  /$$$$$$$$ /$$$$$$  /$$      /$$ /$$$$$$  /$$$$$$ 
+echo          |_  $$_/| $$$ | $$| $$   | $$ /$$__  $$| $$  /$$/| $$_____/       /$$__  $$|__  $$__//$$__  $$| $$$    /$$$|_  $$_/ /$$__  $$
+echo            | $$  | $$$$| $$| $$   | $$| $$  \ $$| $$ /$$/ | $$            | $$  \ $$   | $$  | $$  \ $$| $$$$  /$$$$  | $$  | $$  \__/
+echo            | $$  | $$ $$ $$|  $$ / $$/| $$  | $$| $$$$$/  | $$$$$         | $$$$$$$$   | $$  | $$  | $$| $$ $$/$$ $$  | $$  | $$      
+echo            | $$  | $$  $$$$ \  $$ $$/ | $$  | $$| $$  $$  | $$__/         | $$__  $$   | $$  | $$  | $$| $$  $$$| $$  | $$  | $$      
+echo            | $$  | $$\  $$$  \  $$$/  | $$  | $$| $$\  $$ | $$            | $$  | $$   | $$  | $$  | $$| $$\  $ | $$  | $$  | $$    $$
+echo           /$$$$$$| $$ \  $$   \  $/   |  $$$$$$/| $$ \  $$| $$$$$$$$      | $$  | $$   | $$  |  $$$$$$/| $$ \/  | $$ /$$$$$$|  $$$$$$/
+echo          |______/|__/  \__/    \_/     \______/ |__/  \__/|________/      |__/  |__/   |__/   \______/ |__/     |__/|______/ \______/
+echo
+echo          LOADING . . .
+echo
+echo          Please wait, this could take a few minutes
 echo 
 pwsh -Command "IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing); Install-AtomicRedTeam -getAtomics" -Force
 echo "Script completed" > /tmp/install_remote_d.log
+echo
+echo
+echo
