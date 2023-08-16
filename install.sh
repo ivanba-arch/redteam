47a2d9b587350ecac78a3884e0b98594062e0393
Alan Bautista <41350198+ivanba-arch@users.noreply.github.com>
2023-08-16 02:55:33 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,4 +8,5 @@ pwsh -Command "IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-at
 sudo mkdir -p /AtomicRedTeam/atomics/
 sudo cp -r TR-Blogs/atomic-red-docker/atomics-overrides /AtomicRedTeam/atomics
 sudo cp TR-Blogs/atomic-red-docker/RunTests.ps1 /root
+echo 'pwsh -Command "IEX (IWR ''https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1'' -UseBasicParsing); Install-AtomicRedTeam -getAtomics" -Force' >> /home/ubuntu/.bashrc
 echo "Script completed" > /tmp/script_status.log
